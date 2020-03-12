d1da6a2596d557ce123592ef69367baf7efaec05
pavanlion <pavangonuguntla55@gmail.com>
2020-03-12 15:49:21 +0530

    Updates

---
+++ precache-manifest.12938cb4330a54cb57163d58b11508f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "199add8386d3c4e2be471ac199a722d7",
+    "url": "/project1/index.html"
+  },
+  {
+    "revision": "18453a657ed9ffbbfa7c",
+    "url": "/project1/static/css/main.9062e24c.chunk.css"
+  },
+  {
+    "revision": "34c93cf2613d906da67d",
+    "url": "/project1/static/js/2.6eef2b5d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/project1/static/js/2.6eef2b5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18453a657ed9ffbbfa7c",
+    "url": "/project1/static/js/main.626ecaff.chunk.js"
+  },
+  {
+    "revision": "fe46e28af8f8da747adb",
+    "url": "/project1/static/js/runtime-main.25af23f5.js"
+  },
+  {
+    "revision": "efe01b90316cc1eca182c240aa38b1f8",
+    "url": "/project1/static/media/boy.efe01b90.svg"
+  }
+]);
